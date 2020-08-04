783e1eb92e7aa76437982a34d1082eb5fc74ff42
Felipe <cabezafelipe@gmail.com>
2020-08-04 15:39:38 -0500

    docs agregado

---
+++ docs/precache-manifest.10563da43cc6814af1269e7bac1c34bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733f163394458a1455acd97cb074580d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ce5785cb3af6e46c3fd",
+    "url": "/static/css/main.213ef196.chunk.css"
+  },
+  {
+    "revision": "9948515d998a14a1480d",
+    "url": "/static/js/2.6a91807f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a91807f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ce5785cb3af6e46c3fd",
+    "url": "/static/js/main.8929acef.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
