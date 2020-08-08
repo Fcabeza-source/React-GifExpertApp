434659023ff2ba8cfcc1516ebf3fd7abd165ffd3
Felipe <cabezafelipe@gmail.com>
2020-08-07 21:03:33 -0500

    Updates

---
+++ precache-manifest.937bab176ac8ec0bbc32ca815a4bc36e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732129e96a655ebb89b2f4b9fd34493d",
+    "url": "/React-GifExpertApp/index.html"
+  },
+  {
+    "revision": "60be9a2160e2eb306986",
+    "url": "/React-GifExpertApp/static/css/main.904a5e1f.chunk.css"
+  },
+  {
+    "revision": "ee916409e1ff673ff626",
+    "url": "/React-GifExpertApp/static/js/2.6c7d1144.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-GifExpertApp/static/js/2.6c7d1144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60be9a2160e2eb306986",
+    "url": "/React-GifExpertApp/static/js/main.83e236d7.chunk.js"
+  },
+  {
+    "revision": "6d55b15e0e7ec99b5b0c",
+    "url": "/React-GifExpertApp/static/js/runtime-main.90330878.js"
+  }
+]);
